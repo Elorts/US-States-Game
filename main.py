6e8704745248ffb00ad8db9a9e020641cd5db5a0
Deividas Strole <deiwis@yahoo.com>
2023-03-16 13:43:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+#read file
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
 
 guessed_states = []
 
+#manage user entry and state placement on the map
 while len(guessed_states) < 50:
 
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
